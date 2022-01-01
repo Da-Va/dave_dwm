653e9de914bd0a8d1dbc583c6aa0420509b71ecf
Dave <david1valouch@gmail.com>
2022-01-01 12:34:51 +0100

    pre-patch smart borders

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayonleft = 0;   	/* 0: systray in the right corner, >0: systray on left of status text */
@@ -10,29 +10,36 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-//static const char *fonts[]          = { "monospace:size=10" };
-static const char *fonts[]          = { "Code New Roman Nerd Font:size=10" };
+// static const char *fonts[]          = { "monospace:size=10" };
+// static const char *fonts[]          = { "Code New Roman Nerd Font:size=10" };
+static const char *fonts[]          = { "Consolas NF:size=10" };
 // static const char dmenufont[]       = "monospace:size=10"; 
-static const char dmenufont[]       = "Code New Roman Nerd Font:size=10";
+// static const char dmenufont[]       = "Code New Roman Nerd Font:size=10";
+static const char dmenufont[]       = "Consolas NF:size=10";
+/* Colors */
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
+static const char col_orange[]        = "#c50f1f";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	// [SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_orange  },
 };
 
 static const char *const autostart[] = {
-	"xss-lock", "slocktheme", NULL,
+	"light-locker", NULL,
+	"nitrogen", "--restore", NULL,
 	"/home/dave/.cargo/bin/dwm-status", "/home/dave/.config/dwm-status/config.yml", NULL,
 	"nm-applet", NULL,
 	"udiskie", "-t", NULL,
 	"autorandr", "-c", NULL,
-	"pasystray", NULL,
+	"pasystray", "-a", NULL,
 	"blueman-applet", NULL,
+	"redshift-gtk", NULL,
 	NULL /* terminate */
 };
 
@@ -76,11 +83,11 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "alacritty", NULL };
+static const char *termcmd[]  = { "kitty", NULL };
 
-static const char *slockcmd[] = { "slocktheme", NULL };
+static const char *lockcmd[] = { "dm-tool", "lock", NULL };
 static const char *bukucmd[] = { "buku_dmenu", NULL };
-static const char *filecmd[] = { "alacritty", "-e", "nnn", NULL };
+static const char *filecmd[] = { "kitty", "-e", "nnn", NULL };
 /* sound */
 static const char *mutetogglecmd[] = {"amixer", "set", "Master", "toggle", NULL};
 static const char *volraisecmd[] = {"amixer", "set", "Master", "2%+", NULL};
@@ -92,7 +99,7 @@ static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_Escape, spawn,          {.v = slockcmd } },
+	{ MODKEY,                       XK_Escape, spawn,          {.v = lockcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -144,7 +151,7 @@ static Key keys[] = {
 	{ MODKEY,                   XK_semicolon,  spawn,          {.v = keylayoutcmd } },
 	/* utils */
 	{ MODKEY,                   XK_backslash,  spawn,          {.v = bukucmd } },
-	{ MODKEY,                   XK_n,          spawn,          {.v = filecmd } },
+	{ MODKEY,                   XK_e,          spawn,          {.v = filecmd } },
 
 };
 
